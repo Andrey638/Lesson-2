a59233488f4a287188a2b488b34197dcbd667ea2
Andrey3544 <box4435@mail.ru>
2022-12-08 00:01:31 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿//принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Введите трехзначное число");
+    while(true)
+    {
+        string input = Console.ReadLine();
+            Console.WriteLine("{0}->{1}",input, input[1]);
+            break;
+    }
