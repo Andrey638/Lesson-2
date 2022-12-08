cf4c72018045f1bcc641c11095b5ac4a5f49d690
Andrey3544 <box4435@mail.ru>
2022-12-08 16:21:46 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿//программа принимает на вход цифру, обозначающую день недели, и 
+//проверяет, является ли этот день выходным
+
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 6)
+    {
+        Console.WriteLine("Рабочий день");
+    }    
+    else
+        {
+            Console.WriteLine("Выходной день");
+        }
+ 
