dd5378257cff60a2882e7ca8a0b4d6308db0ee79
Andrey3544 <box4435@mail.ru>
2022-12-08 10:28:53 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿//которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите трехзначное число");
+    while(true)
+    {
+        string input = Console.ReadLine();
+    if (input.Length < 3) 
+            Console.WriteLine("число не трёхзначное");
+        else
+            Console.WriteLine("{0}->{1}",input, input[2]);
+            break;   
+    }
